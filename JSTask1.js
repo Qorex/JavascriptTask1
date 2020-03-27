c7ffc61d098e346e2f84931563b555096d721ce8
Qorex <60553486+Qorex@users.noreply.github.com>
2020-03-27 23:50:06 +0100

    Add files via upload

---
+++ JSTask1.js
@@ -0,0 +1,15 @@
+// Store Name in a Variable
+let name = 'Korede Oduwole';
+
+// Store courses in a Variable
+let courses = ['html', 'css', 'javascript', 'nodejs', 'python', 'csharp', 'java', 'kotlin', 'flutter'];
+
+// Display name and courses.
+console.log('My name is: ' + name);
+console.log('My courses are listed below:');
+console.log(courses);
+
+// Find and display odd numbers from 1 to 200 inclusive
+for (let i = 1; i <= 200; i++) {
+    if (i % 2 !==0) console.log(i);
+}
